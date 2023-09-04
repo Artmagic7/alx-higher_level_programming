e55c21fc5d98383ec40580003648568858c31a4c
Arthur Owens <arth.owens@gmail.com>
2023-09-04 11:42:55 -0700

    Python Update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[:61] + '\n' + str[61:])
-print(str)
+        language that combines remarkable power with very clear syntax"
+print(str.split()[5][:-2] + str.split()[2] + str.split()[7][:6])
