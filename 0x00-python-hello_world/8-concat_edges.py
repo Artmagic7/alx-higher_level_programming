bd207d1256771dcee4d6d8120ab90d2a1bb68628
Arthur Owens <arth.owens@gmail.com>
2023-09-04 11:14:22 -0700

    Python Update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,3 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[:61] + '\n' + str[61:])
+print(str)
