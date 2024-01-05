5f21e8633b3c9b7ba13922d225b03d9b6d617323
Arthur Owens <arth.owens@gmail.com>
2024-01-05 06:04:37 -0800

    Python - Network #0

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Sending a JSON POST request to a URL and displays the body of the response
-filename=$2
-if [ -f "$filename" ]; then
-    curl -sX POST -H "Content-Type: application/json" -d @"$filename" "$1"
-else
-    echo "Not a valid JSON"
-fi
+# Script sending a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
