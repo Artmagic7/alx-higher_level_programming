57793440aebcd3ea32eb8baa16f80bcd3e58d90f
Arthur Owens <arth.owens@gmail.com>
2024-01-07 01:02:31 -0800

    Python - Network #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,15 +2,14 @@
 """ This takes in a URL, sends a request to the URL, and displays the body of the response.
 If the HTTP status code is greater than or equal to 400, print: Error code: followed by the value of the HTTP status code """
 
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
 
+if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
