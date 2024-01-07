f0803af772c835a852af7061c20e6870f35148ac
Arthur Owens <arth.owens@gmail.com>
2024-01-07 01:25:48 -0800

    Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,10 +5,7 @@ and finally displays the body of the response. """
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
 
-    data = {'email': email}
-    response = requests.post(url, data=data)
-    print("Your email is:", response.text)
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
